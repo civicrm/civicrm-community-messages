eb6568f81a926918eb0dd578a6dce5a3bf64b495
Tim Otten <totten@civicrm.org>
2013-04-07 05:07:13 +0000

    Change url from /alerts to the spec'd /alert

--- src/CRM/CommunityMessagesBundle/Tests/Controller/DefaultControllerTest.php
+++ src/CRM/CommunityMessagesBundle/Tests/Controller/DefaultControllerTest.php
@@ -8,8 +8,8 @@ class DefaultControllerTest extends WebTestCase {
 
   public function goodRequests() {
     return array(
-      array('/alerts?prot=1&ver=4.2.1&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd1234'),
-      array('/alerts?prot=1&ver=4.3.beta5&uf=Drupal6&sid=abcd1234abcd1234abcd1234abcd1234'),
+      array('/alert?prot=1&ver=4.2.1&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd1234'),
+      array('/alert?prot=1&ver=4.3.beta5&uf=Drupal6&sid=abcd1234abcd1234abcd1234abcd1234'),
     );
   }
 
@@ -31,12 +31,12 @@ class DefaultControllerTest extends WebTestCase {
 
   public function badRequests() {
     return array(
-      array('/alerts'),
-      array('/alerts?prot=2&ver=4.3.1&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd1234'), // bad prot
-      array('/alerts?prot=1&ver=4.3.zeta&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd1234'), // bad ver
-      array('/alerts?prot=1&ver=4.3.1&uf=Drupal5&sid=abcd1234abcd1234abcd1234abcd1234'), // bad cms
-      array('/alerts?prot=1&ver=4.3.1&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd123'), // short sid
-      array('/alerts?prot=1&ver=4.3.1&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd1234a'), // long sid
+      array('/alert'),
+      array('/alert?prot=2&ver=4.3.1&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd1234'), // bad prot
+      array('/alert?prot=1&ver=4.3.zeta&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd1234'), // bad ver
+      array('/alert?prot=1&ver=4.3.1&uf=Drupal5&sid=abcd1234abcd1234abcd1234abcd1234'), // bad cms
+      array('/alert?prot=1&ver=4.3.1&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd123'), // short sid
+      array('/alert?prot=1&ver=4.3.1&uf=Drupal&sid=abcd1234abcd1234abcd1234abcd1234a'), // long sid
     );
   }
 
