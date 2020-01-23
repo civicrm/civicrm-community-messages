2aa07c56296ae68b241c0dd780d2abd4d0c39cd4
Mathieu Lutfy <mathieu@symbiotic.coop>
2020-01-23 11:21:26 -0500

    CiviCRM Resources: Add translation support.

--- src/CRM/CommunityMessagesBundle/Controller/WelcomeController.php
+++ src/CRM/CommunityMessagesBundle/Controller/WelcomeController.php
@@ -1,6 +1,6 @@
 <?php
 // Testing URL:
-// https://alert.civicrm.org/welcome?prot=1&ver=4.6.6&uf=UnitTests&sid=12345678901234567890123456789012&en_US&co=1013
+// https://alert.civicrm.org/welcome?prot=1&ver=4.6.6&uf=UnitTests&sid=12345678901234567890123456789012&lang=en_US&co=1013
 // Change parameters as needed
 
 namespace CRM\CommunityMessagesBundle\Controller;
@@ -19,6 +19,7 @@ class WelcomeController extends Controller {
       'sid' => '/^(test_mode|[a-zA-Z0-9]{32})$/',
       'uf' => '/^(Backdrop|Drupal|Drupal6|Drupal8|WordPress|Joomla|UnitTests)$/',
       'ver' => '/^([0-9\.]|alpha|beta|dev|rc){2,12}$/',
+      'lang' => '/^([a-z]{2}_[A-Z]{2})/',
     );
 
     $params = array();
@@ -36,6 +37,9 @@ class WelcomeController extends Controller {
     $this->createRequestLog($params);
     $this->updateSidSummary($params['sid'], $params['ver'], $params['uf']);
 
+    // Setup translation
+    $this->initGettext($params);
+
     // Construct response
     $document = $this->gettingStarted($params);
     return $this->renderHtml($document);
@@ -47,6 +51,17 @@ class WelcomeController extends Controller {
     return array(
       'error' => $message,
     );
+  }
+
+  public function initGettext($params) {
+    if (empty($params['lang']) || $params['lang'] == 'en_US') {
+      return;
+    }
+
+    setlocale(LC_ALL, $params['lang'] . '.utf8');
+    bindtextdomain("alerts", $_SERVER["DOCUMENT_ROOT"] . "/../l10n/");
+    bind_textdomain_codeset('alerts', 'UTF-8');
+    textdomain("alerts");
   }
 
   public function createRequestFailure() {
@@ -101,16 +116,16 @@ class WelcomeController extends Controller {
 
   public function gettingStarted($params) {
     $sections = array(
-      'Configure and extend' => array(
+      $this->ts('Configure and extend') => array(
         'Admin Console' => array(
           'icon' => 'fa-list-ol',
           'img' => 'align-left',
-          'html' => '<a href="{crmurl.configbackend}">Configuration checklist</a>',
+          'html' => '<a href="{crmurl.configbackend}">' . $this->ts('Configuration checklist') . '</a>',
         ),
         'Extensions Dir' => array(
           'icon' => 'fa-puzzle-piece',
           'img' => 'puzzle-piece',
-          'html' => '<a href="https://civicrm.org/extensions?src=gs" target="_blank">Enhance CiviCRM with extensions</a>',
+          'html' => '<a href="https://civicrm.org/extensions?src=gs" target="_blank">' . $this->ts('Enhance CiviCRM with extensions') . '</a>',
         ),
         'CiviConnect' => array(
           'icon' => 'fa-plug',
@@ -120,61 +135,67 @@ class WelcomeController extends Controller {
         'Documentation' => array(
           'icon' => 'fa-book',
           'img' => 'book',
-          'html' => '<a href="https://civicrm.org/documentation?src=gs" target="_blank">Review CiviCRM documentation</a>',
+          'html' => '<a href="https://civicrm.org/documentation?src=gs" target="_blank">' . $this->ts('Review CiviCRM documentation') . '</a>',
         ),
       ),
-      'Get support' => array(
+      $this->ts('Get support') => array(
         'Chat' => array(
           'icon' => 'fa-comments-o',
           'img' => 'chat',
-          'html' => '<a href="https://chat.civicrm.org?src=gs" target="_blank">Jump in and chat with the community</a>',
+          'html' => '<a href="https://chat.civicrm.org?src=gs" target="_blank">' . $this->ts('Jump in and chat with the community') . '</a>',
         ),
         'StackExchange' => array(
           'icon' => 'fa-question',
           'img' => 'question-mark',
-          'html' => '<a href="http://civicrm.stackexchange.com" target="_blank">Ask a question on Stack Exchange</a>',
+          'html' => '<a href="http://civicrm.stackexchange.com" target="_blank">' . $this->ts('Ask a question on Stack Exchange') . '</a>',
         ),
         'Trainings' => array(
           'icon' => 'fa-microphone',
           'img' => 'microphone',
-          'html' => '<a href="https://civicrm.org/upcoming-events?src=gs" target="_blank">Find upcoming trainings</a>',
+          'html' => '<a href="https://civicrm.org/upcoming-events?src=gs" target="_blank">' . $this->ts('Find upcoming trainings') . '</a>',
         ),
         'Experts' => array(
           'icon' => 'fa-user-md',
           'img' => 'people',
-          'html' => '<a href="https://civicrm.org/experts?src=gs " target="_blank">Get support from the CiviCRM experts</a>',
+          'html' => '<a href="https://civicrm.org/experts?src=gs " target="_blank">' . $this->ts('Get support from the CiviCRM experts') . '</a>',
         ),
       ),
-      'Get involved' => array(
+      $this->ts('Get involved') => array(
         'Register' => array(
           'icon' => 'fa-sign-in',
           'img' => 'monitor',
-          'html' => '<a href="https://civicrm.org/user/register?src=gs" target="_blank">Register with</a> or <a href="https://civicrm.org/user?src=gs" target="_blank">log into</a> CiviCRM.org',
+          'html' => $this->ts('<a %1>Register with</a> or <a %2>log into</a> CiviCRM.org', [
+            1 => 'href="https://civicrm.org/user/register?src=gs" target="_blank"',
+            2 => 'href="https://civicrm.org/user?src=gs" target="_blank"',
+          ]),
         ),
         'Preferences' => array(
           'icon' => 'fa-check-square-o',
           'img' => 'magnifying-glass',
-          'html' => '<a href="https://civicrm.org/update-my-mailing-preferences" target="_blank">Manage your preferences with CiviCRM</a>',
+          'html' => '<a href="https://civicrm.org/update-my-mailing-preferences" target="_blank">' . $this->ts('Manage your preferences with CiviCRM') . '</a>',
         ),
         'Register your site' => array(
           'icon' => 'fa-flag',
           'img' => 'flag',
-          'html' => '<a href="https://civicrm.org/register-your-site?src=gs&sid=' . $params['sid'] . '" target="_blank">Register your site with CiviCRM</a>',
+          'html' => '<a href="https://civicrm.org/register-your-site?src=gs&sid=' . $params['sid'] . '" target="_blank">' . $this->ts('Register your site with CiviCRM') . '</a>',
         ),
         'Meetups' => array(
           'icon' => 'fa-users',
           'img' => 'map',
-          'html' => '<a href="https://civicrm.org/meet-ups?src=gs" target="_blank">Find a meetup in your area</a>',
+          'html' => '<a href="https://civicrm.org/meet-ups?src=gs" target="_blank">' . $this->ts('Find a meetup in your area') . '</a>',
         ),
         'Become a member' => array(
           'icon' => 'fa-user-plus',
           'img' => 'person',
-          'html' => '<a href="https://civicrm.org/become-a-member?src=gs&sid=' . $params['sid'] . '" target="_blank">Become a member</a> (<a href="http://civicrm.org/member-benefits?src=gs" target="_blank">review member benefits</a>)',
+          'html' => $this->ts('<a %1>Become a member</a> (<a %2>review member benefits</a>)', [
+            1 => 'href="https://civicrm.org/become-a-member?src=gs&sid=' . $params['sid'] . '" target="_blank"',
+            2 => 'href="http://civicrm.org/member-benefits?src=gs" target="_blank"',
+          ]),
         ),
         'Events' => array(
           'icon' => 'fa-calendar',
           'img' => 'clock',
-          'html' => '<a href="https://civicrm.org/events?src=gs" target="_blank">View all CiviCRM events</a>',
+          'html' => '<a href="https://civicrm.org/events?src=gs" target="_blank">' . $this->ts('View all CiviCRM events') . '</a>',
         ),
       ),
     );
@@ -184,8 +205,20 @@ class WelcomeController extends Controller {
     unset($sections['Configure and extend']['CiviConnect']);
 
     // Header
+    $support_url = 'https://civicrm.org/get-involved?src=gs';
+    $donate_url = 'https://civicrm.org/civicrm/contribute/transact?reset=1&id=47&src=gs';
+
+    if (!empty($params['lang']) && $params['lang'] != 'en_US') {
+      $donate_url .= '&lcMessages=' . $params['lang'];
+    }
+
     $output = '<div class="crm-block crm-content-block">';
-    $output .= "<div id=\"help\">Created by contributors around the world, CiviCRM is, at its heart, built to impact the world. As open source software, CiviCRM is made freely available for use and improvement, serving organizations whose missions collectively impact and improve the world in which we live. Please <a href='https://civicrm.org/civicrm/contribute/transact?reset=1&id=47&src=gs' target='_blank'>donate to support the project</a> and see <a href='https://civicrm.org/get-involved?src=gs' target='_blank'>other ways to get involved</a>!</div>";
+    $output .= "<div id=\"help\">";
+    $output .= $this->ts("Created by contributors around the world, CiviCRM is, at its heart, built to impact the world. As open source software, CiviCRM is made freely available for use and improvement, serving organizations whose missions collectively impact and improve the world in which we live. Please <a %1>donate to support the project</a> and see <a %2>other ways to get involved</a>!", [
+      '1' => "href='$donate_url' target='_blank'",
+      '2' => "href='$support_url' target='_blank'",
+    ]);
+    $output .= "</div>";
 
     // Sections
     foreach ($sections as $title => $items) {
@@ -223,6 +256,41 @@ class WelcomeController extends Controller {
       }
     }
     return $activeSites;
+  }
+
+  /**
+   * @param $string
+   * @return string
+   */
+  public function ts($string, $params = []) {
+    $string = gettext($string);
+    return $this->strarg($string, $params);
+  }
+
+  /**
+   * Replace arguments in a string with their values. Arguments are represented by % followed by their number.
+   *
+   * @param string $str
+   *   source string.
+   *
+   * @return string
+   *   modified string
+   */
+  public function strarg($str, $params) {
+    if (empty($params)) {
+      return $str;
+    }
+
+    $tr = [];
+    $p = 0;
+
+    foreach ($params as $key => $val) {
+      if (is_numeric($key)) {
+        $tr['%' . ++$p] = $val;
+      }
+    }
+
+    return strtr($str, $tr);
   }
 
 }
